b2fcce4f3f35fdbda5c03f46e8a30b69b985d75a
PromisT <nyamunda17@gmail.com>
2018-06-26 14:19:06 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,50 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+teamA = 0;
+teamB = 0;
+
+
 
   constructor(public navCtrl: NavController) {
 
+    
   }
+  onclick(){
+    this.teamA = 0;
+    this.teamB = 0;
+  }
+
+onclickone(){
+  this.teamA += 3;
+  console.log(this.teamA);
+}
+
+onclicktwo(){
+  this.teamA += 2;
+  console.log(this.teamA);
+  
+}
+  
+onclickthree(){
+  this.teamA += 1;
+  console.log(this.teamA);
+  
+}
+onclickfour(){
+  this.teamB += 3;
+  console.log(this.teamB);
+  
+}
+onclickfive(){
+  this.teamB += 2;
+  console.log(this.teamB);
+  
+}
+onclicksix(){
+  this.teamB += 1;
+  console.log(this.teamB);
+  
+}
 
 }
